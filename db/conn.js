b11a36415561c45797848acfcec8b4c4c13306c6
ashwanikumar <ashwanikumar01011997123@gmail.com>
2023-08-08 17:04:57 +0530

    commit

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-const DB=process.env.MONGO_URL;
+const DB='mongodb+srv://ashwani:ashwani@cluster0.z1gmjnk.mongodb.net/userData?retryWrites=true&w=majority';
 
 mongoose.connect(DB).then(()=>{
     console.log("connected succesfully");
